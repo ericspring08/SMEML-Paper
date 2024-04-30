46cb4a4a3580e20796a1161daa0224567f3db0c3
Eric Zhang <ericspring08@gmail.com>
2024-04-30 13:46:17 -0500

    flaml working

--- FLAML/flame.py
+++ FLAML/flame.py
@@ -1,24 +1,27 @@
 from flaml import AutoML
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
 import pandas as pd
+import pickle
 
 # Load your dataset
 dataset_path = 'heart.csv'  # Specify the path to your dataset file
-data = pd.read_csv(dataset_path)d
+data = pd.read_csv(dataset_path)
 
 X = data.drop(columns=["target"])  # Adjust the column name as per your dataset
 y = data["target"]  # Adjust the column name as per your dataset
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42)
 
 # Customize FLAML's AutoML
 automl = AutoML()
 automl_settings = {
-    "time_budget": 200,  # Set the maximum time budget for AutoML in seconds
-    "task": "classification",  # Specify the type of task (classification or regression)
+    "time_budget": 60,  # Set the maximum time budget for AutoML in seconds
+    # Specify the type of task (classification or regression)
+    "task": "classification",
     "metric": "accuracy",  # Choose the evaluation metric to optimize
     "learner_selector": "auto",  # Let FLAML automatically select the learners
-    "ensemble_size": 5,  # Set the size of the ensemble model
     # Add more parameters as needed
 }
 
@@ -31,3 +34,7 @@ y_pred = automl.predict(X_test)
 # Evaluate the model's performance
 accuracy = accuracy_score(y_test, y_pred)
 print("Accuracy:", accuracy)
+
+# save the model
+with open('model.pkl', 'wb') as model_pkl:
+    pickle.dump(automl, model_pkl)
