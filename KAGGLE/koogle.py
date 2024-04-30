95f34c2109abcd010283bca591db8c2b8a7f3983
Surrealzz <91282211+Surrealzz@users.noreply.github.com>
2024-04-30 17:28:06 +0000

    kaggle working

--- KAGGLE/koogle.py
+++ KAGGLE/koogle.py
@@ -1,9 +1,14 @@
 from kaggle.api.kaggle_api_extended import KaggleApi
 import os
+
+# Set the full path to the directory containing kaggle.json
 os.environ['KAGGLE_CONFIG_DIR'] = '/workspaces/MTF-PAPER/KAGGLE/'
 
 # Initialize the Kaggle API
 api = KaggleApi()
+
+# Authenticate with your Kaggle credentials
+api.authenticate()
 
 # Search for datasets by keyword
 datasets = api.dataset_list(search='disease')
